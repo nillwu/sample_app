f3aff87d8c3ab896093faebf755250b991ff0a0b
Nill Wu <nill.wu@gmail.com>
2014-01-26 02:28:20 +0800

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
